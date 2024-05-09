975d5bc1dce2546dd16623e80f2409647a022570
FelipeSanchezC <felipesanchez9600@hotmail.com>
2024-05-08 20:16:35 -0500

    Subir Proyecto

---
+++ js/logica.js
@@ -0,0 +1,9 @@
+function logica(){
+    var numeros = ["Cristian", "David", "Carlos", "Miguel", "Andres"]
+
+    //Ciclo For 
+    for(let i = 0; i < numeros.length; i++){
+        console.log(numeros[i])
+    }
+}
+
---
+++ js/tablas.js
@@ -0,0 +1,57 @@
+function tablas(){
+    
+    let alumnosMAtriculados = [
+        {
+            "rut":"1-9",
+            "nombre":"Pedro",
+            "apellido":"Perez",
+            "cod_curso":"AE600",
+            "descripcion":"Algoritmos y Estructuras de datos"
+        },
+        {
+            "rut":"2-7",
+            "nombre":"Juan",
+            "apellido":"Jara",
+            "cod_curso":"BD253",
+            "descripcion":"Bases de datos"
+        },
+    ]
+
+    let alumnos = [
+        {
+            "rut":"1-9",
+            "nombre":"Pedro",
+            "apellido":"Perez"
+        },
+        {
+            "rut":"2-7",
+            "nombre":"Juan",
+            "apellido":"Jara"
+        }
+    ]
+
+    let curso = [
+        {
+            "cod_curso":"AE600",
+            "descripcion":"Algoritmos y Estructuras de datos"
+        },
+        {
+            "cod_curso":"BD253",
+            "descripcion":"Bases de datos"
+        }
+    ]
+
+    let matricula = [
+        {
+            "rut":"1-9",
+            "cod_curso":"AE600"
+        },
+        {
+            "rut":"2-7",
+            "cod_curso":"BD253"
+        },
+    ]
+
+    console.log(alumnos[0])
+    console.log(alumnosMAtriculados[0])
+}
